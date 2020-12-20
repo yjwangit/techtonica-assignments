2cd5418b17b7ae9552ad534e8dc0083b47b790a6
Yingjie Wang <jewangyj@gmail.com>
2020-12-20 17:54:10 -0600

    var to let

--- 1.js
+++ 1.js
@@ -12,46 +12,51 @@
 // Data Types and Variables
 
 // Exercise 1. Simply declare a variable named 'emptyVariable'
-var emptyVariable
+let emptyVariable;
 
 
 // Exercise 2. Declare a variable called 'myName'
-var myName
+let myName;
 
 
 // Exercise 3. Assign your name as the value for 'myName'
-myName= "Yingjie"
+myName= "Yingjie";
 
 
 // Exercise 4. Declare a variable called dreamDestination and give it the value
 // of what your dream vacation destination is.
-
+let dreamDestination = "Hawaii";
 
 
 // Exercise 5. Make the following variables and assignments
 // - a variable called num1 with a value greater than 1 and less than 10
 // - a variable called num2 with a value of greater than or equal to 1 and less
 // than or equal to 10
-
+let num1;
+let num2;
 
 
 // Exercise 6. Write a comment below explaining the difference between the rules
 // for num1 and num2 from Exercise 5. What numbers would be valid values for 
 // num2 but not num1?
-//
+//num1 >1 && num1 <10; num2 >=1 && num2 <= 10. 
+//1 and 10 are the valid value for num2 but not num1.
+
 
 
 // Exercise 7. Now we will try some addition.
 // a. Assign the the values of 4 and 6 to num1 and num2, respectively.
 // b. Make a new variable called 'theSum', and use 'num1' and
 // 'num2' to assign its value using the "+" operator.
-
+num1 = 4;
+num2 = 6;
+let theSum = num1 + num2;
 
 
 // Exercise 8. Now we will try some multiplication.
 // Make a new variable called 'theProduct', multiply num1 and num2 and assign
 // the resulting value to 'theProduct'.
-
+let theProduct = num1 * num2;
 
 
 // Exercise 9.
@@ -59,7 +64,7 @@ myName= "Yingjie"
 // 'dreamDestination' and the concatenation method of your choice, make the
 // value of statement to be:
 // "Hi, my name is <your name>, and I can't wait to visit <your destination>!"
-
+let statement = "Hi, my name is "+ myName +", and I can't wait to visit " + dreamDestination+"!";
 
 
 // *NOTE* For the following exercises, put your answers in the line below
@@ -71,17 +76,17 @@ myName= "Yingjie"
 // Exercise 10. What is the data type of the value stored in 'dreamDestination',
 // place your answer in the quotes below:
 
-const myAnswerForExercise10 = "";
+const myAnswerForExercise10 = "string";
 
 // Exercise 11. What is the data type of the value stored in 'product',
 // place your answer in the quotes below:
 
-const myAnswerForExercise11 = "";
+const myAnswerForExercise11 = "number";
 
 // Exercise 12. What do you think the value of emptyVariable is?
 // Place your answer below:
 
-const myAnswerForExercise12 = ;
+const myAnswerForExercise12 = undefined;
 
 // Exercise 13. If
 const A = "R";
@@ -90,36 +95,36 @@ const C = 4;
 const D = "D";
 
 // What is the value of B + C
-const myAnswerForExercise13 = ;
+const myAnswerForExercise13 = 5;
 
 // Exercise 14 What is the value of
 // A + "n" + D
-const myAnswerForExercise14 = ;
+const myAnswerForExercise14 = "RnD";
 
 // Exercise 15 What is the value of
 // A + B * 2 + D + C / 2:
-const myAnswerForExercise15 = ;
+const myAnswerForExercise15 = "R2D2" ;
 
 // Exercise 16 What is the value of A * B
-const myAnswerForExercise16 = ;
+const myAnswerForExercise16 = NaN;
 
 // Exercise 17 What is the value of true || false
-const myAnswerForExercise17 = ;
+const myAnswerForExercise17 = true;
 
 // Exercise 18 What is the value of 1 === "1"
-const myAnswerForExercise18 = ;
+const myAnswerForExercise18 = false;
 
 // Exercise 19 What is the value of 1 == "1"
-const myAnswerForExercise19 = ;
+const myAnswerForExercise19 = true;
 
 // Exercise 20 What is the value of 1 < 1
-const myAnswerForExercise20 = ;
+const myAnswerForExercise20 = false;
 
 // Exercise 21 What is the value of 1 < 2 < 3
-const myAnswerForExercise21 = ;
+const myAnswerForExercise21 = true;
 
 // Exercise 22 What is the value of 2 < 1 < 3
-const myAnswerForExercise22 = ;
+const myAnswerForExercise22 = false;
 
 // Congrats, you made it to the end!
 // Did you find this easy or hard? If you used references, which ones helped you? 
